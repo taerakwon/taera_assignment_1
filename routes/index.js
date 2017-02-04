2c3998e897c21c0d14db1698e3bd4b84826f1802
taerakwon <taerakwon@gmail.com>
2017-02-04 10:51:38 -0500

    Added navigation bar to view templates (home, about, projects)

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 /**
  *  Author: Taera Kwon (300755802)
- *  File Name: index.js
+ *  File Name: routes/index.js
  *  Description: This is where all routing gets done
  */
 
 let express = require('express');
+/* Rexpress Route */
 let router = express.Router();
 
 /* GET home page. */
@@ -13,6 +14,7 @@ router.get('/', (req, res, next) => {
   let currentYear = currentDate.getFullYear();
   let currentMonth = currentDate.getMonth();
   let currentDay = currentDate.getDay();
+  /* To renter view */
   res.render('index', { 
     title: "Taera Kwon's Express Portfolio Site",
     date: currentYear + "-" + currentMonth + "-" + currentDay
@@ -21,11 +23,13 @@ router.get('/', (req, res, next) => {
 
 /* GET about page. */
 router.get('/about', (req, res, next) => {
+  /* To renter view */
   res.render('about', { title: 'About Me'});
 });
 
 /* GET projects page. */
 router.get('/projects', (req, res, next) => {
+  /* To renter view */
   res.render('projects', { title: 'Projects'});
 });
 
