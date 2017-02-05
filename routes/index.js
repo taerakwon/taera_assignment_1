fa382ab8f23a7f58b9a4e5165e5c3bd5e113d58e
taerakwon <taerakwon@gmail.com>
2017-02-04 22:41:06 -0500

    Added partial and contents folder.  Partial contains header and foot, and contents contain body contents

--- routes/index.js
+++ routes/index.js
@@ -10,39 +10,34 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  let currentDate = new Date();
-  let currentYear = currentDate.getFullYear();
-  let currentMonth = currentDate.getMonth();
-  let currentDay = currentDate.getDay();
   /* To renter view */
-  res.render('index', { 
-    title: "Taera Kwon's Express Portfolio Site",
-    date: currentYear + "-" + currentMonth + "-" + currentDay
+  res.render('contents/index', { 
+    title: "Taera Kwon's Website"
   });
 });
 
 /* GET about page. */
 router.get('/about', (req, res, next) => {
   /* To renter view */
-  res.render('about', { title: 'About Me'});
+  res.render('contents/about', { title: 'About Me'});
 });
 
 /* GET projects page. */
 router.get('/projects', (req, res, next) => {
   /* To renter view */
-  res.render('projects', { title: 'Projects'});
+  res.render('contents/projects', { title: 'Projects'});
 });
 
 /* GET services page. */
 router.get('/services', (req, res, next) => {
   /* To renter view */
-  res.render('services', { title: 'Services'});
+  res.render('contents/services', { title: 'Services'});
 });
 
 /* GET contact me page. */
 router.get('/contact', (req, res, next) => {
   /* To renter view */
-  res.render('contact', { title: 'Contact Me'});
+  res.render('contents/contact', { title: 'Contact Me'});
 });
 
 // Exports router object
